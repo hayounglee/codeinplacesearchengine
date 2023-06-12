34809ad2e137d956db71919f3fb2417981dd9f07
hayounglee <hayoung.lee@gmail.com>
2023-06-12 17:14:07 +0900

    Add files via upload

---
+++ common_elements.py
@@ -0,0 +1,43 @@
+"""
+File: common_elements.py
+------------------------
+File to implement a function that is passed two lists and returns a new list
+containing those elements that appear in both of the lists passed in.
+"""
+
+
+def common(list1, list2):
+    """
+    This function is passed two lists and returns a new list containing
+    those elements that appear in both of the lists passed in.
+    >>> common(['a'], ['a'])
+    ['a']
+    >>> common(['a', 'b', 'c'], ['x', 'a', 'z', 'c'])
+    ['a', 'c']
+    >>> common(['a', 'b', 'c'], ['x', 'y', 'z'])
+    []
+    >>> common(['a', 'a', 'b'], ['a', 'a', 'x'])
+    ['a']
+    """
+    ret = []
+    for element in list1:
+        if element in list2 and element not in ret:
+                ret.append(element)
+
+    return ret
+    """
+    You implement this function.  Don't forget to remove the 'pass' statement above.
+    """
+
+
+def main():
+    # Same tests as the doctests above, but can be run from the terminal:
+    # python3 common_elements.py
+    print(common(['a'], ['a']))                             # should print ['a']
+    print(common(['a', 'b', 'c'], ['x', 'a', 'z', 'c']))    # should print ['a', 'c']
+    print(common(['a', 'b', 'c'], ['x', 'y', 'z']))         # should print []
+    print(common(['a', 'a', 'b'], ['a', 'a', 'x']))         # should print ['a']
+
+
+if __name__ == '__main__':
+    main()
